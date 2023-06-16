706cfc07ca55604351e1e035ba4193ff94722a92
Hermann Degenhardt <hermanndegenhardt@gmail.com>
2023-06-16 19:24:04 -0400

    update example

--- examples/create_inference_plots.py
+++ examples/create_inference_plots.py
@@ -8,7 +8,7 @@ from inference import create_inference_analysis, inference_cluster_analysis
 # Create inference plots and extract metric results
 output_path = "output"                                                           # The output folder
 data_path = "../data/full_data"                                                  # Folder where the image data are located
-gt_annotations = "../../old/cluster/split_test/validation_annotations.json"          # File containing the GT annotations
+gt_annotations = "../../old/cluster/split_test/validation_annotations.json"      # File containing the GT annotations
 inf_annotations = "../../6_full_200_split/inference/coco_instances_results.json" # File containing the inference annotations
 quiet_mode = True                                                                # Enable/disable information outputs
 size_filter = 25                                                                 # Filter detection objects smaller than <size> square pixels
